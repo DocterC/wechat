15fb265ca45f5e19e8a11199e19ff4789e06e980
liudong <liudong@xmmakeit.com>
2017-04-20 22:43:15 +0800

    up

--- app/Http/Controllers/Api/DemoController.php
+++ app/Http/Controllers/Api/DemoController.php
@@ -18,6 +18,7 @@ class DemoController extends Controller
         $result = $request->all();
         $openid = $request['openid'];
         $time =time();
+        /*
         return  "<xml>
                 <ToUserName>$openid</ToUserName>
                 <FromUserName>292808514</FromUserName>
@@ -25,6 +26,27 @@ class DemoController extends Controller
                 <MsgType>text</MsgType>
                 <Content>hello world</Content>
                 </xml>";
+            */
+
+        $file_in = file_get_contents("php://input"); //接收post数据
+
+        $xml = simplexml_load_string($file_in);//转换post数据为simplexml对象
+
+        foreach($xml->children() as $child)    //遍历所有节点数据
+        {
+
+            $str = $child->getName() . ": " . $child . "<br />"; //打印节点名称和节点值
+            Log::info($str);
+
+//if($child->getName()=="from")    //捡取要操作的节点
+//{
+//echo "i say ". ": get you!" . "<br />"; //操作节点数据
+//}
+
+        }
+
+        return 'success';
+
     }
 
     public function demo()
@@ -38,6 +60,8 @@ class DemoController extends Controller
 
         return $arr;
     }
+
+
 
 
 }
